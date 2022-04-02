189ffee0a3935f74d447aad71bc7ea4c44a26d59
timdog19 <102239203+timdog19@users.noreply.github.com>
2022-04-02 22:14:14 +0300

    Update Stats.js

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,13 +1,11 @@
-import React, {useRef, useState, useEffect} from 'react'
+import React, {useRef, useEffect} from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import axios from 'axios';
-import '../styles/Stats.css'
+import '../styles/About.css'
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { CSSRulePlugin } from "gsap/CSSRulePlugin";
-
 
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(CSSRulePlugin);
@@ -15,82 +13,33 @@ gsap.registerPlugin(CSSRulePlugin);
 const styles = theme => ({
     root: {
       flexGrow: 1,
+    },
+    imagebreakpoint: {
+        [theme.breakpoints.only("xs")]: {
+            display: "none"
+          },
+          [theme.breakpoints.down("sm")]: {
+            left: "-85px"
+          },
+          [theme.breakpoints.up("md")]: {
+            height: "25rem !important"
+          },
     }
   });
 
-function Stats(props) {
+function About(props) {
     const { classes } = props;
 
-    let stats = useRef(null)
-    let items = useRef(null)
-    let owner = useRef(null)
-    let floorprice = useRef(null)
-    let volumetraded = useRef(null)
-    let triggerelement = useRef(null)
+    
+    let aboutelment = useRef(null)
 
     useEffect(()=> {
-        gsap.from(stats,{
+        gsap.from(aboutelment,{
             scrollTrigger:{
-                trigger: triggerelement,
-                endTrigger: triggerelement,
+                trigger: aboutelment,
+                endTrigger: aboutelment,
                 toggleActions: "restart pause restart pause",
-                start: "35% 95%",
-                end: "100% 5%",
-                markers: false
-            },
-            duration: 2,
-            opacity: 0,
-            y: 100,
-            ease: "expo.out"
-        });
-        gsap.from(items,{
-            scrollTrigger:{
-                trigger: triggerelement,
-                endTrigger: triggerelement,
-                toggleActions: "restart pause restart pause",
-                start: "35% 95%",
-                end: "100% 5%",
-                markers: false
-            },
-            duration: 2,
-            opacity: 0,
-            y: 100,
-            ease: "expo.out"
-        });
-        gsap.from(owner,{
-            scrollTrigger:{
-                trigger: triggerelement,
-                endTrigger: triggerelement,
-                toggleActions: "restart pause restart pause",
-                start: "35% 95%",
-                end: "100% 5%",
-                markers: false
-            },
-            duration: 2,
-            opacity: 0,
-            y: 100,
-            ease: "expo.out"
-        });
-        gsap.from(floorprice,{
-            scrollTrigger:{
-                trigger: triggerelement,
-                endTrigger: triggerelement,
-                toggleActions: "restart pause restart pause",
-                start: "35% 95%",
-                end: "100% 5%",
-                markers: false
-            },
-            duration: 2,
-            opacity: 0,
-            y: 100,
-            ease: "expo.out"
-        });
-        gsap.from(volumetraded,{
-            scrollTrigger:{
-                trigger: triggerelement,
-                endTrigger: triggerelement,
-                toggleActions: "restart pause restart pause",
-                start: "35% 95%",
+                start: "top 100%",
                 end: "100% 5%",
                 markers: false
             },
@@ -100,83 +49,26 @@ function Stats(props) {
             ease: "expo.out"
         });
     }, [])
-    
-    
-
-    const url = 'https://api.opensea.io/api/v1/collections?asset_owner=0x2a1ea3ac74703c4efbf09252d6e859296344de5e&offset=0';
-    const [collection, setCollection] = useState(null);
-
-    useEffect(()=> {
-        axios.get(url)
-        .then(response => {
-            setCollection(response.data)
-        })
-    }, [url])
-
-    let itemsData = null
-    let ownerData = null
-    let floorPriceData = 0
-    let volumeTradedData = 0
-    if(collection){
-        itemsData = collection[0].stats.count;
-        ownerData = collection[0].stats.num_owners
-        floorPriceData = Math.round((collection[0].stats.floor_price)*100)/100
-        volumeTradedData = Math.round((collection[0].stats.total_volume)*100)/100
-    }
 
     return (
-        <div id="Stats" className="stats-wrapper" style={{"position": "relative"}}>
-            <div className="custom-container" style={{"paddingBlock" : "7rem"}} ref={el => {triggerelement = el}}>
-                <Grid container spacing={3} style={{ "margin-bottom": "1.5rem"}}>
-                    <Grid item xs={12} style={{"marginBottom" : "5rem"}}>
-                        <Typography align="start" variant="h3" gutterBottom ref={el => {stats = el}}>
-                            Stats
-                        </Typography>
+        <div id="About" className="wrapper" style={{"position": "relative"}}>
+                <div className="custom-container py-7" ref={el => {aboutelment = el}}>
+                    <Grid container spacing={40}>
+                        <Grid item xs={12} sm={6} style={{ "display" : "flex", "align-self": "center", "justifyContent" : "start" }} >
+                            <img src="../images/3.png" alt="nft" style={{ "position" : "absolute", "width" : "auto", "height" : "25rem", "bottom" : "0" }} className={classes.imagebreakpoint}/>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <Typography variant="h3" className={classes.grow} >
+                                ABOUT US
+                            </Typography>
+                            <div style={{ "margin-top": "1rem", "text-transform": "uppercase" }}><Typography variant="body1" className={classes.grow}>
+                                We are a small group of software and game developers that decided to create a special NFT for the best P2E Game, "SneakPunks". Get it now to join the SneakPunks and get access to our upcoming game.
+                            </Typography></div>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12} sm={6} md={3}>
-                        <div ref={el => {items = el}}>
-                            <Typography align="center" gutterBottom variant="subtitle1" component="div">
-                                Items
-                            </Typography>
-                            <Typography align="center" variant="h6" gutterBottom>
-                                {itemsData}
-                            </Typography>
-                        </div>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={3}>
-                        <div ref={el => {owner = el}}>
-                            <Typography align="center" gutterBottom variant="subtitle1" component="div">
-                                Owner
-                            </Typography>
-                            <Typography align="center" variant="h6" gutterBottom>
-                                {ownerData}
-                            </Typography>
-                        </div>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={3}>
-                        <div ref={el => {floorprice = el}}>
-                            <Typography align="center" gutterBottom variant="subtitle1" component="div">
-                                Floor price
-                            </Typography>
-                            <Typography align="center" variant="h6" gutterBottom>
-                                {floorPriceData} eth
-                            </Typography>
-                        </div>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={3}>
-                        <div ref={el => {volumetraded = el}}>
-                            <Typography align="center" gutterBottom variant="subtitle1" component="div">
-                                Volume traded
-                            </Typography>
-                            <Typography align="center" variant="h6" gutterBottom>
-                                {volumeTradedData} eth
-                            </Typography>
-                        </div>
-                    </Grid>
-                </Grid>
+                </div>
             </div>
-        </div>
     )
 }
 
-export default withStyles(styles)(Stats)
+export default withStyles(styles)(About)
